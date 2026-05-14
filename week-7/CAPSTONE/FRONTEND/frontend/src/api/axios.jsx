a2bc334cdb83c549ffa0607196d6820e179438b5
vikramdingari0-cloud <vikramdingari0@gmail.com>
2026-05-15 02:32:36 +0530

    chore: updated frontend api link to production

--- week-7/CAPSTONE/FRONTEND/frontend/src/api/axios.jsx
+++ week-7/CAPSTONE/FRONTEND/frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://capstone-backend-hesv.onrender.com",
   withCredentials: true,
 });
 
